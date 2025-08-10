510e56a7c3638f771cab89ecdbba4c7051ac1639
Shriram Jagdhane <shriramjagdhane2004@gmail.com>
2025-08-10 11:48:16 +0530

    Utills code understanding

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandeler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err => next(err)))
+    }
+}
+
+
+
+
+
+export {asyncHandeler}
+
+
+
+
+
+
+/*
+const asyncHandeler = (fn) => async (req, res, rext) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
