f0d79fd89f8d4b550c9d124736922c0d0b9308ed
Shriram Jagdhane <shriramjagdhane2004@gmail.com>
2025-08-17 08:18:22 +0530

    added missing code

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -17,6 +18,11 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+
+router.route("/login").post(loginUser)
+
+router.route("/logouy").post(verifyJWT, logoutUser)
 
 
 export default router
