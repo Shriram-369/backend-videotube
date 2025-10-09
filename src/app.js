a5d05a32a1abfd2ecde66197d53f79bd0fd69d32
Shriram Jagdhane <shriramjagdhane2004@gmail.com>
2025-10-09 18:30:56 +0530

    Add healthcheck functionality with routes and controller

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 //routes declaration
 app.use("/api/v1/users",userRouter)   //https://localhost:8000/api/v1/users/register
@@ -30,5 +31,6 @@ app.use("/api/v1/video", videoRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/tweet",tweetRouter)
 app.use("/api/v1/like",likeRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export { app }
